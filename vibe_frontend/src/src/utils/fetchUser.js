4100f37112500f2f36f24e2e0fc9d9c657c4dbaf
Akhil Gangwar <60934750+Akhil38@users.noreply.github.com>
2022-05-27 20:02:24 +0530

    Delete vibe_frontend/src directory

--- vibe_frontend/src/src/utils/fetchUser.js
+++
@@ -1,3 +0,0 @@
-export const fetchUser = () => {
-    return localStorage.getItem('user') !== 'undefined'?JSON.parse(localStorage.getItem("user")) : localStorage.clear() // clear when something went wrong like user token expired
-}
